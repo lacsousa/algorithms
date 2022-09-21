a506c21541d481119a4d5e56f090a5f9d0e9356a
Luciano Cordeiro <lacs@petrobras.com.br>
2022-09-21 17:43:57 -0300

    New solution - Remove duplicates in arrays

---
+++ javatests/RemoveDuplicatesLC.java
@@ -0,0 +1,24 @@
+package javatests;
+
+class RemoveDuplicatesLC {
+  public static void main(String[] args) {
+
+    int[] arr = { 0,0,1,1,1,2,2,3,3,4 };
+    removeDuplicates(arr);
+
+  }
+
+  public static int removeDuplicates(int[] nums) {
+
+    int k = nums.length > 0 ? 1 : 0;
+
+    for (int elem : nums) {
+      if (elem > nums[k - 1]) {
+        nums[k++] = elem;
+      }
+    }
+    System.out.println("k = " + k) ;
+    return k;
+   
+  }
+}
